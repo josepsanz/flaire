3b5d8c2d459959207c9a9bd9b4f9eb17705551dc
Josep Maria Sanz Xicola <jsanz83@gmail.com>
2025-08-23 21:31:40 +0200

    SQLAlchemy + alembic

---
+++ docs/alembic.md
@@ -0,0 +1,53 @@
+# Alembic
+
+## Initial workflow
+
+1. Create the models. You can ask any GenAI to generate them for you in SQLAlchemy format.
+2. Initialize `alembic` for the first time:
+
+```sh
+alembic init migrations
+```
+
+This creates an `alembic.ini` and a `migrations` directory:
+``` sh 
+migrations
+├── env.py
+├── README
+├── script.py.mako
+└── versions
+```
+
+Edit `alembic.ini` and `migrations/env.py` as needed for your project. 
+
+3. Run the following commands (for PostgreSQL and MariaDB, the database must already be created):
+```
+alembic revision --autogenerate -m "initial schema"
+alembic upgrade head
+```
+
+This is going to create all empty tables with the indicated schema.
+
+Optional step:
+
+```python
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+from flaire.models import Base
+
+DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/perfumes"
+engine = create_engine(DATABASE_URL)
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+Base.metadata.create_all(engine)
+```
+
+## Upgrade workflow
+
+``` sh
+alembic revision --autogenerate -m "Example changing hash size column in perfums table"
+alembic upgrade head
+```
---
+++ src/flaire/models/__init__.py
@@ -0,0 +1,41 @@
+import enum
+import datetime
+
+import sqlalchemy as sa
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class PerfumeType(enum.Enum):
+    edt = 'edt'
+    edp = 'edp'
+    parfum = 'parfum'
+    elixir = 'elixir'
+    extract = 'extract'
+
+class Perfume(Base):
+    __tablename__ = 'perfums'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, nullable=False)
+    type = sa.Column(sa.Enum(PerfumeType), nullable=False)
+    size = sa.Column(sa.Integer, nullable=False)
+    hash = sa.Column(sa.String(32), nullable=False, unique=True)  # md5 hash hex string
+
+    prices = relationship('Price', back_populates='perfume', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<Perfume(name={self.name}, type={self.type}, size={self.size})>'
+
+class Price(Base):
+    __tablename__ = 'prices'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    perfume_id = sa.Column(sa.Integer, sa.ForeignKey('perfums.id', ondelete='CASCADE'), nullable=False)
+    ts = sa.Column(sa.DateTime, default=datetime.datetime.now, nullable=False)
+    price = sa.Column(sa.Integer, nullable=False)
+
+    perfume = relationship('Perfume', back_populates='prices')
+
+    def __repr__(self):
+        return f'<Price(perfume_id={self.perfume_id}, ts={self.ts}, price={self.price})>'
